e241d9a06956e3d7b65c7d3b802a4963f2c64577
Maaz Ur Rahman <118599615+maaz123123@users.noreply.github.com>
2023-11-13 22:05:35 +0500

    Simple Calculator. ccp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int num1, num2;
+    int sum, difference, product, division, remainder, average;
+
+    cout << "enter 1st number" << endl;
+    cin >> num1;
+
+    cout << "enter 2nd number " << endl;
+    cin >> num2;
+
+    return 0;
+}
